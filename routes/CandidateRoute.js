b8e119c943ca2327fa969719f0e0a6eba5d2e3cf
gauravraj2601meepl <gauravr@meepl.day>
2025-01-30 14:38:40 +0530

    module change

--- routes/CandidateRoute.js
+++ routes/CandidateRoute.js
@@ -1,12 +1,12 @@
 
 const express = require("express");
-
+const Candidate_Module = require("../models/WhatsappBusiness/Candidate");
 const candidateRouter = express.Router();
 
 
 candidateRouter.get("/", async (req, res) => {
     try {
-        const candidate_data = await Candidate.find();
+        const candidate_data = await Candidate_Module.find();
         res.status(200).json(candidate_data);
     } catch (error) {
         console.error("Error fetching users:", error);
