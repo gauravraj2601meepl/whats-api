79357f079f46ec1f6a5fc5be208ba4a6e743807a
gauravraj2601meepl <gauravr@meepl.day>
2025-02-03 13:06:44 +0530

    changes

--- middleware/Whatsapp/Onboard.Validation.js
+++ middleware/Whatsapp/Onboard.Validation.js
@@ -17,7 +17,6 @@ module.exports = {
     },
     WhatsappOnboardAddValidation: async (req, res, next) => {
         const value = await WhatsappAddCandidate.validate(req.body);
-        console.log('value', value)
         if (value?.error) {
             console.log("validation-error",{ message: value.error.details[0].message, errorcode: 400 })
             res?.status(400).json({
